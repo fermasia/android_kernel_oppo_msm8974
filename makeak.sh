3ffd847e4ef60fc0fac93dd1bf75d872e1a5ee5b
fermasia <fermasia@hotmail.com>
2015-04-26 20:58:17 -0300

    Automate jobcount in buildscript

--- makeak.sh
+++ makeak.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TOOLCHAIN="/home/nando/dev/toolchains/uber49/bin"
+JOBS="-j$(grep -c ^processor /proc/cpuinfo)"
 echo "Cleaning old files"
 rm -f ../AnykernelOMNI/dtb
 rm -f ../AnykernelOMNI/zImage
@@ -11,7 +12,7 @@ make clean && make mrproper
 export ARCH=arm
 export SUBARCH=arm
 make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- msm8974_find7op_defconfig
-make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- -j2
+make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- $JOBS
 echo "End of compiling kernel!"
 
 DATE_END=$(date +"%s")
