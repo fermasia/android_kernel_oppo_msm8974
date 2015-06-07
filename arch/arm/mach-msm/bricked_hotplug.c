7138ccf661b82338caedbdfa914669f218d340bb
fermasia <fermasia@hotmail.com>
2015-06-07 16:40:09 -0300

    Tune BRICKED HOTPLUG

--- arch/arm/mach-msm/bricked_hotplug.c
+++ arch/arm/mach-msm/bricked_hotplug.c
@@ -83,7 +83,7 @@ static struct cpu_hotplug {
 	.bricked_enabled = HOTPLUG_ENABLED,
 };
 
-static unsigned int NwNs_Threshold[8] = {12, 0, 20, 7, 25, 10, 0, 18};
+static unsigned int NwNs_Threshold[8] = {11, 0, 22, 6, 28, 11, 0, 16};
 static unsigned int TwTs_Threshold[8] = {140, 0, 140, 190, 140, 190, 0, 190};
 
 struct down_lock {
