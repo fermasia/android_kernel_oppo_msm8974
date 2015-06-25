8affcbf4205383c5a45651df311db4048f2d9ed9
Zachariah <zkennedy87@gmail.com>
2015-06-25 15:43:57 -0400

    State_Notifier: Update Function for Backlight Control
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- drivers/video/backlight/lm3630_bl.c
+++ drivers/video/backlight/lm3630_bl.c
@@ -456,11 +456,11 @@ int lm3630_bank_a_update_status(u32 bl_level)
 #ifdef CONFIG_STATE_NOTIFIER
 	// if display is switched off
 	if (!use_fb_notifier && bl_level == 0)
-		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+		state_suspend();
 
 	// if display is switched on
 	if (!use_fb_notifier && bl_level != 0 && pre_brightness == 0)
-		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+		state_resume();
 #endif
 
 /* Xiaori.Yuan@Mobile Phone Software Dept.Driver, 2014/04/28  Add for add log for 14001 black screen */
