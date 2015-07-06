2249311d01929c3223207402fb2b9345a90fb7eb
DerRomtester <dieb.stefan.96@gmail.com>
2015-07-06 20:35:07 +0200

    gcc: fix some compiler warnings with 5.1 and above
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>
    
    Conflicts:
    	arch/arm/kernel/return_address.c

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -58,8 +58,8 @@ void *return_address(unsigned int level)
 
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
 
+
 #if 0
-#warning "TODO: return_address should use unwind tables"
 #endif
 
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
