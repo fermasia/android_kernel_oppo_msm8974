5c03a56cbe4d47a47726fbff8f7009717b63d70f
Sultanxda <sultanxda@gmail.com>
2015-09-25 17:42:51 -0700

    power: qpnp-charger: Restore charge current to max after thermal throttle
    When thermal throttling is over and we're back to a normal operating temperature, the charge current is not restored to what it was before throttling (max_bat_chg_current).
    
    Set the charge current back to the max instead of 1200mA when thermal mitigation is finished in order to fix the slow-charge issue.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    Conflicts:
    	drivers/power/qpnp-charger.c

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -6598,7 +6598,11 @@ static int handle_batt_temp_normal(struct qpnp_chg_chip *chip)
 				if(chip->aicl_current == 0) {
 					soft_aicl(chip);
 				} else {
-					if(chip->aicl_current == 2000) {
+					if (chip->aicl_current >= 1500) {
+#ifdef CONFIG_MACH_MSM8974_14001
+/* OPPO 2014-06-03 sjc Modify for Find7op temp rising problem */
+						qpnp_chg_iusbmax_set(chip, chip->max_bat_chg_current);
+#else
 						qpnp_chg_iusbmax_set(chip, 1500);
 					} else {
 						qpnp_chg_iusbmax_set(chip, chip->aicl_current);
