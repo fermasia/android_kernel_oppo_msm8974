54f9e0dd20baaee3a303f8add9d42b39c1d22c78
fermasia <fermasia@hotmail.com>
2015-11-29 18:24:34 -0300

    fix derp

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -612,11 +612,7 @@ module_param(pvs_config_ver, uint, S_IRUGO);
 
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 
-<<<<<<< HEAD
-#define CPU_VDD_MIN	 600
-=======
 #define CPU_VDD_MIN	 475
->>>>>>> 7f76089... Voltage Control: added voltage control for DTS based kernels
 #define CPU_VDD_MAX	1450
 
 extern bool is_used_by_scaling(unsigned int freq);
