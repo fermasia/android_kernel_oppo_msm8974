cf8400e52bd6d3601a156feac171d154261f2203
fermasia <fermasia@hotmail.com>
2015-12-05 16:25:55 -0300

    Buildscript: adapt for wlan modules handling

--- makeak.sh
+++ makeak.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-TOOLCHAIN="/home/nando/dev/toolchains/linaro52a15/bin"
+TOOLCHAIN="/home/nando/dev/toolchains/linaro49a15/bin"
 JOBS="-j$(grep -c ^processor /proc/cpuinfo)"
 echo "Cleaning old files"
 rm -f ../AnykernelOMNI/dtb
 rm -f ../AnykernelOMNI/zImage
+rm -f ../AnykernelOMNI/modules/wlan.ko
 echo "Making kernel"
 DATE_START=$(date +"%s")
 
@@ -11,8 +12,8 @@ make clean && make mrproper
 
 export ARCH=arm
 export SUBARCH=arm
-make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- msm8974_find7op_defconfig
-make CROSS_COMPILE=$TOOLCHAIN/arm-eabi- $JOBS
+make CROSS_COMPILE=$TOOLCHAIN/arm-cortex_a15-linux-gnueabihf- msm8974_find7op_defconfig
+make CROSS_COMPILE=$TOOLCHAIN/arm-cortex_a15-linux-gnueabihf- $JOBS
 echo "End of compiling kernel!"
 
 DATE_END=$(date +"%s")
@@ -22,6 +23,7 @@ echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 
 ../ramdisk_one_plus_one/dtbToolCM -2 -o ../AnykernelOMNI/dtb -s 2048 -p ../omni/scripts/dtc/ ../omni/arch/arm/boot/
 cp arch/arm/boot/zImage ../AnykernelOMNI/zImage
+cp drivers/staging/prima/wlan.ko ../AnykernelOMNI/modules/wlan.ko
 cd ../AnykernelOMNI/
 zipfile="DONKEY-V"$1".zip"
 zip -r -9 $zipfile *
